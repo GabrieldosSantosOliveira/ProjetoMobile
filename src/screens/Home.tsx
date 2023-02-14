f75fa4f50bd0729ff48b9f65c46165d9c2a214f5
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-14 13:20:54 -0400

    feat: add screen Home

---
+++ src/screens/Home.tsx
@@ -0,0 +1,29 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+import { Input } from '../components/Input';
+export const Home = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Input
+        LeftElement={<FontAwesome name="bank" />}
+        autoComplete="name-given"
+      />
+      <Input
+        textContentType="password"
+        LeftElement={<FontAwesome size={15} name="bank" />}
+        autoComplete="password-new"
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+});
