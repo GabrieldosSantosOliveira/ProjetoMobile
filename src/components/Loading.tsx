3831e398cfb0e85b05b11a7227df452b255895b5
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-16 10:29:38 -0400

    feat: change theme for dark or light

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,25 @@
+import { StatusBar } from '@components/StatusBar';
+import { useTheme } from '@hooks/useTheme';
 import { ActivityIndicator, View } from 'react-native';
+
 export const Loading = () => {
+  const { colorMode } = useTheme();
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <ActivityIndicator size="large" />
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colorMode === 'dark' ? '#141c30' : '#f5f8ff',
+      }}
+    >
+      <StatusBar
+        backgroundColor={colorMode === 'dark' ? '#141c30' : '#f5f8ff'}
+      />
+      <ActivityIndicator
+        size="large"
+        color={colorMode === 'dark' ? '#f5f8ff' : '#141c30'}
+      />
     </View>
   );
 };
